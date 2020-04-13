7685c5661af63b87c7846bb83f2aa43b5362daec
puneet <puneetkant@gmail.com>
2020-04-13 15:07:51 +0530

    greetings added

--- DemoWebAppPS/DemoWebAppPS/Controllers/HomeController.cs
+++ DemoWebAppPS/DemoWebAppPS/Controllers/HomeController.cs
@@ -6,21 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DemoWebAppPS.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace DemoWebAppPS.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this._configuration = configuration;
         }
+
+        //public HomeController(IConfiguration configuration)
+        //{
+        //    this._configuration = configuration;
+        //}
 
         public IActionResult Index()
         {
-            return View();
+            var model = _configuration["Greeting"];
+            return View("Index", model);
         }
 
         public IActionResult Privacy()
